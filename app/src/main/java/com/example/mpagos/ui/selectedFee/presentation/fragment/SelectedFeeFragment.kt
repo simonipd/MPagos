439a9cc20ba3c67e6d22e76ff10971deb8c650f3
simonipd <2012inca1E18>
2022-06-06 17:26:25 -0400

    Small refactor SelectedFeeFragment.kt

--- app/src/main/java/com/example/mpagos/ui/selectedFee/presentation/fragment/SelectedFeeFragment.kt
+++ app/src/main/java/com/example/mpagos/ui/selectedFee/presentation/fragment/SelectedFeeFragment.kt
@@ -2,19 +2,16 @@ package com.example.mpagos.ui.selectedFee.presentation.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.mpagos.R
 import com.example.mpagos.databinding.FragmentSelectedFeeBinding
-import com.example.mpagos.ui.main.domain.model.PaymentMethodElement
 import com.example.mpagos.ui.selectedBank.domain.model.Bank
-import com.example.mpagos.ui.selectedFee.domain.model.Fee
-import com.example.mpagos.ui.selectedFee.presentation.adapter.SelectedAdapter
+import com.example.mpagos.ui.selectedFee.util.selectedItem
+import com.example.mpagos.ui.selectedFee.util.validateFee
 import com.example.mpagos.ui.util.FunctionsUtils.Companion.launchAndCollect
 import com.example.mpagos.ui.util.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,7 +35,6 @@ class SelectedFeeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         if (_binding == null) {
-            activity?.actionBar?.setTitle(R.string.fragment_title_selected_bank)
             _binding = FragmentSelectedFeeBinding.inflate(inflater)
             binding?.root?.context.also { mContext = it!! }
             viewModel.getFee()
@@ -53,42 +49,4 @@ class SelectedFeeFragment : Fragment() {
             validateFee(it.paymentMethod, it.bank, it.listFee)
         }
     }
-
-    private fun validateFee(
-        paymentMethod: PaymentMethodElement?,
-        bank: Bank?,
-        listFee: List<Fee>?
-    ) {
-        val findListfee =
-            listFee?.find { it.paymentMethodID.equals(paymentMethod?.id) && it.issuer.id.equals(bank?.id) }
-        findListfee?.let {
-            val feeAdapter = SelectedAdapter(binding!!.root.context, findListfee.payerCosts, findListfee.issuer.secureThumbnail)
-            binding!!.spinnerSelectedMethod.adapter = feeAdapter
-        }
-        Log.i("validateFee", findListfee.toString())
-    }
-
-    private fun selectedItem() {
-        binding!!.spinnerSelectedMethod.onItemSelectedListener = (object :
-            AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                view?.let {
-                    _listBank?.let {
-                        viewModel.setBank(it[position])
-                        binding!!.btnSave.isEnabled = true
-                        parent?.setSelection(position)
-                    }
-                }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-        })
-    }
-
-
 }
