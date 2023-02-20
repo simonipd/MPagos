f1ece4ac28af5d47f554030f3bf37b212c66f354
sperezd <sperezd@smu.cl>
2023-02-20 17:21:32 -0300

    implement test listPaymentMethodElement add libraries: coroutines-core, coroutines-test, and cash.turbine for test with flow

--- app/src/test/java/com/example/mpagos/util/presentation/viewmodel/MainViewModelTest.kt
+++ app/src/test/java/com/example/mpagos/util/presentation/viewmodel/MainViewModelTest.kt
@@ -1,15 +1,20 @@
 package com.example.mpagos.util.presentation.viewmodel
 
+import app.cash.turbine.test
 import com.example.mpagos.ui.main.domain.model.PaymentMethodElement
 import com.example.mpagos.ui.main.domain.usecase.GetPaymentMethodUseCase
 import com.example.mpagos.ui.selectedBank.domain.usecase.GetBankUseCase
 import com.example.mpagos.ui.selectedPayerCost.domain.usecase.GetPayerCostUseCase
+import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
 
@@ -29,12 +34,21 @@ class MainViewModelTest {
 
     private lateinit var vm: MainViewModel
 
-    val listPaymentMethodElement = listOf<PaymentMethodElement>()
+    private val listPaymentMethodElement = listOf<PaymentMethodElement>()
 
     @Before
     suspend fun setup() {
         whenever(getPaymentMethodUseCase()).thenReturn(listPaymentMethodElement)
         vm = MainViewModel(getPaymentMethodUseCase, getBankUseCase, getPayerCost)
+    }
+
+    @Test
+    fun getBanks() = runTest {
+        vm.state.test {
+            assertEquals(MainViewModel.UiState(), awaitItem())
+            assertEquals(MainViewModel.UiState(listPaymentMethodElement = listPaymentMethodElement), awaitItem())
+            cancel()
+        }
     }
 
 
